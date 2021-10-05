04a0b89a99c147838c0bdf4638a509cd253fc87f
namank03 <khandelwalnaman03@gmail.com>
2021-10-06 01:59:17 +0530

    Best sum memorization - deep copy is required while assigning or returning anything from the memo dict

---
+++ dynamic_programming/best_sum_memorization.py
@@ -0,0 +1,38 @@
+import copy
+
+
+def bestSum(target, num_arr, memo={}):
+    if target in memo:
+        return copy.deepcopy(memo[target])
+
+    if target < 0:
+        return None
+
+    if target == 0:
+        return []
+
+    # ! here the for loop is trying out all the possible combinations. So, it's good idea to take the shortestCombination variable outside the for loop to track the shortestCombination
+
+    shortestCombination = None
+
+    for i in num_arr:
+        remainder = target - i
+        remainderResult = bestSum(remainder, num_arr, memo)
+        if remainderResult is not None:
+            # ! We'll get inside this condition only when there is a path. Now, we need to see which path is the best
+            # The below line will generate the path
+            remainderResult.append(i)
+            combination = remainderResult
+
+            # Logic to evaluate the shortestCombination possible. Notice we not returning early here. Even if we found the path. We're re-running the for-loop to evaluate all possible path and finally we're selecting the shortestCombination out of all
+            if shortestCombination is None or len(shortestCombination) > len(
+                combination
+            ):
+                shortestCombination = combination
+
+    memo[target] = copy.deepcopy(shortestCombination)
+    return shortestCombination
+
+
+result = bestSum(7, [2, 4, 1, 3,7])
+print(result)
