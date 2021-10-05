4ff3ea676f3f58285ded830468ffb7dcbce27c77
namank03 <khandelwalnaman03@gmail.com>
2021-10-05 10:39:11 +0530

    has_path_df optimized with seen

---
+++ graphs/has_path_df_optimized.py
@@ -0,0 +1,32 @@
+graph = {
+    'a': ['d'],
+    'd': ['k', 'f'],
+    'l': ['d'],
+    'f': ['o'],
+    'g': ['f'],
+    'o': ['n'],
+    'k': ['h', 'o'],
+    'h': [],
+    'n': [],
+}
+
+
+seen = []
+
+# * Here we are further optimizing the haspath with the seen array. If the node is traversed before and the path is not found, So there is no need to traverse the node again and we can safely move to the next neighour in line
+
+
+def hasPathDF(graph, source, destination):
+    seen.append(source)
+    # The below will serve as the base case
+    if source == destination:
+        return True
+    for neighbor in graph[source]:
+        if neighbor not in seen and hasPathDF(graph, neighbor, destination):
+            # ! TIP: We can include a if condition in the recursive call to return early here we're saying if one of the recursive call hit the base condition and returns true to the call stack that means we've found at least one of the path and we can exit the recursion now
+            return True
+    else:
+        return False
+
+
+print(hasPathDF(graph, 'a', 'g'))
