8532ff04e7781f817db2967cd8953d76217a5988
namank03 <khandelwalnaman03@gmail.com>
2021-10-05 23:06:20 +0530

    how sum with memorization

--- dynamic_programming/how_sum_with_memorization.py
+++ dynamic_programming/how_sum_with_memorization.py
@@ -1,21 +1,23 @@
-def howsum(target, num_arr):
+def howsum(target, num_arr, memo={}):
+    if target in memo:
+        return memo[target]
+
     if target < 0:
         return None
 
     if target == 0:
-        #! whenever we want to evaluate the path or the values used to obtain the final result, we 1st start with the empty list and then add the values to this empty list when the solution is found.
         return []
 
     for i in num_arr:
         # as we have specified in the basecase the return value is either true or false. Meaning if any one of the recursive call hit the base case True then we can stop the recursion and return early.
         remainder = target - i
-        remainderResult = howsum(remainder, num_arr)
+        remainderResult = howsum(remainder, num_arr, memo)
         if remainderResult is not None:
-            #! if remainderResult is non None that means we've evaluated the soultion and now we can add the result
             remainderResult.append(i)
-            return remainderResult
-
-    return None
+            memo[target] = remainderResult
+            return memo[target]
+    memo[target] = None
+    return memo[target]
 
 
 result = howsum(300, [7, 14])
