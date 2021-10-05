afdbdf453e75bde8e2f23f277e0936f9bc735976
namank03 <khandelwalnaman03@gmail.com>
2021-10-05 11:43:56 +0530

    haspath undirected graph - way2

--- graphs/undirected_graph_has_path_df_visited_set.py
+++ graphs/undirected_graph_has_path_df_visited_set.py
@@ -6,14 +6,13 @@ edge_list = [['i', 'j'], ['k', 'i'], ['m', 'k'], ['k', 'l'], ['o', 'n']]
 def hasPathDF(graph, src, dst, visited_set):
     if src == dst:
         return True
-
-    # Base case to optimize and detect cycle alterative way to write this is in has_path_df_optimized.py
-    if src in visited_set:
-        return False
-
     visited_set.add(src)
 
-    return any(hasPathDF(graph, neighbor, dst, visited_set) for neighbor in graph[src])
+    return any(
+        hasPathDF(graph, neighbor, dst, visited_set)
+        for neighbor in graph[src]
+        if neighbor not in visited_set
+    )
 
 
 def check_path(edge_list, node1, node2):
@@ -21,4 +20,4 @@ def check_path(edge_list, node1, node2):
     return hasPathDF(graph, node1, node2, set())
 
 
-print(check_path(edge_list, 'i', 'n'))
+print(check_path(edge_list, 'i', 'l'))
