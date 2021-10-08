5865562b43ed9be2a1f354c7118638bf83698bf3
namank03 <khandelwalnaman03@gmail.com>
2021-10-08 11:10:24 +0530

    LinkedList - added insert/remove functionality

--- linked_list/basic_linked_list.py
+++ linked_list/basic_linked_list.py
@@ -1,3 +1,6 @@
+from os import link
+
+
 class Node:
     def __init__(self, data):
         self.data = data
@@ -9,14 +12,28 @@ class Node:
 
 class LinkedList:
     def __init__(self, capacity):
+        if capacity < 0:
+            raise ValueError("Must be positive")
         self.head = None
         self.tail = None
         self.capacity = capacity
         self.size = 0
 
+    def check_capacity_and_inc_size(func):
+        def inner(self, *args, **kwargs):
+            if self.size >= self.capacity:
+                raise IndexError("Capacity Full")
+            try:
+                func(self, *args, **kwargs)
+            except Exception as e:
+                print(f'Some Unknown Exception {e} Occured')
+
+            self.size += 1
+
+        return inner
+
+    @check_capacity_and_inc_size
     def prepend(self, data):
-        if self.size >= self.capacity:
-            return "Exceeded capacity"
         node = Node(data)
         if self.size == 0:
             self.head = node
@@ -26,15 +43,61 @@ class LinkedList:
             self.head = node
             node.next = temp
 
-        self.size += 1
+    @check_capacity_and_inc_size
+    def append(self, data):
+        node = Node(data)
+        if self.size == 0:
+            self.head = node
+            self.tail = node
+        else:
+            temp = self.tail
+            self.tail = node
+            temp.next = node
+
+    @check_capacity_and_inc_size
+    def insert(self, data, pos):
+        if pos > self.size:
+            raise ValueError(
+                f"Can't remove element from this position, position must be less than current size = {self.size}",
+            )
+        node = Node(data)
+        current = self.head
+        for _ in range(pos - 1):
+            current = current.next
+
+        node.next = current.next
+        current.next = node
+
+    def remove(self, pos):
+        if pos > self.size:
+            raise ValueError(
+                f"Can't remove element from this position, position must be less than current size = {self.size}",
+            )
+
+        previous = self.head
+
+        for _ in range(pos - 1):
+            previous = previous.next
+        current = previous.next
+
+        previous.next = current.next
+        current.next = None
 
     def __str__(self):
         return self.head.__str__()
 
 
-linked_list = LinkedList(4)
+linked_list = LinkedList(10)
 linked_list.prepend(9)
 linked_list.prepend(0)
+print(linked_list.size)
 linked_list.prepend(3)
 linked_list.prepend(10)
+print(linked_list.size)
+linked_list.append(90)
+linked_list.append(100)
+linked_list.insert(4, 4)
+print(linked_list.size)
+print(linked_list)
+linked_list.remove(4)
 print(linked_list)
