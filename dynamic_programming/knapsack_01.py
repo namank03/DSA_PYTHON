86e8cba07e83595755e809d00021d52db4bf6694
namank03 <khandelwalnaman03@gmail.com>
2021-10-08 23:23:19 +0530

    DP: Knapsack01

---
+++ dynamic_programming/knapsack_01.py
@@ -0,0 +1,22 @@
+def knapSack(self, W, wt, val, n, memo={}):
+    if (W, n) in memo.keys():
+        return memo[(W, n)]
+
+    if n == 0:
+        return 0
+
+    if W - wt[n-1] >= 0:
+        memo[(W, n)] = max(val[n-1] + knapSack(wt, val,
+                                               W - wt[n-1], n-1, memo), knapSack(wt, val, W, n-1, memo))
+    else:
+        memo[(W, n)] = knapSack(wt, val, W, n - 1, memo)
+
+    return memo[(W, n)]
+
+
+values = [1, 2, 3]
+weight = [4, 5, 6]
+
+W = 10
+
+print(knapSack(weight, values, W, 3))
