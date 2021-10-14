e1daa80c2985c3ee1018be4bba07c9bdace9261a
namank03 <khandelwalnaman03@gmail.com>
2021-10-14 09:44:34 +0530

    Array - max sum

--- blind_75/max_sum.py
+++ blind_75/max_sum.py
@@ -3,11 +3,11 @@ from itertools import accumulate
 
 def max_sum(nums):
 
-    # Here the trick is to use the kadan's algorithm. keep track of the running sum and update the max sum whenever the running sum goes greater than the max sum
+    # Here the trick is to use the kadan's algorithm. keep track of the running sum and update the max sum whenever the running sum goes greater than the max sum.
 
     max_sum = running_sum = nums[0]
 
-    for i in nums:
+    for i in nums[1:]:
         running_sum += i
         running_sum = max(running_sum, i)
         max_sum = max(max_sum, running_sum)
