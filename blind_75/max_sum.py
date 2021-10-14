0ee2f26307c606a4e99b758779ffe48035af0a87
namank03 <khandelwalnaman03@gmail.com>
2021-10-14 09:41:15 +0530

    Array - max_sum in an array - kadan's algo

---
+++ blind_75/max_sum.py
@@ -0,0 +1,22 @@
+from itertools import accumulate
+
+
+def max_sum(nums):
+
+    # Here the trick is to use the kadan's algorithm. keep track of the running sum and update the max sum whenever the running sum goes greater than the max sum
+
+    max_sum = running_sum = nums[0]
+
+    for i in nums:
+        running_sum += i
+        running_sum = max(running_sum, i)
+        max_sum = max(max_sum, running_sum)
+
+    return max_sum
+
+
+def max_sum_via_acc(nums):
+    return max(accumulate(nums, lambda a, b: max(b, a+b)))
+
+
+print(max_sum([5, 4, -11, 7, 8]))
