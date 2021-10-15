08a1cd94acf842f67cbd4d952b8b953e4f9d4b77
namank03 <khandelwalnaman03@gmail.com>
2021-10-15 17:59:53 +0530

    DP: longest increasing subsequence

--- blind_75/longest_increasing_subsequence.py
+++ blind_75/longest_increasing_subsequence.py
@@ -1,15 +1,23 @@
 # Input: nums = [10,9,2,5,3,7,101,18]
 # Output: 4
 
-def lcs(arr, n):
+def solve(nums, n, memo={}):
 
     if n < 2:
         return 1
 
-    if arr[n-1] > arr[n-2]:
-        return max(1 + lcs(arr, n-1), lcs(arr, n-1))
+    if n in memo:
+        return memo[n]
+
+    # Here we're comparing the element n-1(last) with n-2(second last) if the condition matches we've got 2 choices i.e either to take the element or to not and we need the max of the result
+
+    if nums[n-1] > nums[n-2]:
+        memo[n] = max(1 + solve(nums, n-1, memo), solve(nums, n-1, memo))
     else:
-        return lcs(arr, n-1)
+        memo[n] = solve(nums, n-1, memo)
+
+    return memo[n]
 
 
-print(lcs([1, 10, 4, 3, 7], 5))
+nums = [4, 10, 4, 3, 8, 9]
+solve(nums, len(nums))
