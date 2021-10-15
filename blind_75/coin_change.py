811a439bdc6db28b96fa4a329e1014948e353489
namank03 <khandelwalnaman03@gmail.com>
2021-10-15 16:24:34 +0530

    DP:Min coin required tip-update

--- blind_75/coin_change.py
+++ blind_75/coin_change.py
@@ -2,33 +2,39 @@
 #! 1. Either we can have a no coin means the array is empty. If we have no coins then it's impossible to generate the required sum
 #! 2. If the target sum is 0 then no coin is required to generate the required sum.
 
-# * The above 2 will serve as the base condition and here we'll write the 2nd base condition 1st.
+# * The above 2 will serve as the base condition and here we'll write the 2nd base condition 1st. And as this is min problem so the good starting point is to hold the reverse as ans i.e math.inf
+
 import math
 
 
-def coin_change(coins, amount, memo={}):
+def coinChange(coins, amount):
 
-    if amount == 0:
-        return 0
+    def solve(coins, amount, memo={}):
 
-    if len(coins) == 0 or amount < 0:
-        return math.inf
+        if amount == 0:
+            return 0
 
-    if amount in memo:
-        return memo[amount]
+        if len(coins) == 0 or amount < 0:
+            return math.inf
 
-    min_coins = math.inf
+        if amount in memo:
+            return memo[amount]
 
-    for i in coins:
-        result = coin_change(coins, amount-i, memo)
-        if result != math.inf:
-            result += 1
-            if min_coins == math.inf or min_coins > result:
-                min_coins = result
+        min_coins = math.inf
 
-    memo[amount] = min_coins
-    return min_coins
+        for i in coins:
+            result = solve(coins, amount-i, memo)
+            if result != math.inf:
+                result += 1
+                if min_coins == math.inf or min_coins > result:
+                    min_coins = result
 
+        memo[amount] = min_coins
+        return min_coins
 
-print(coin_change([2], 3))
-# vs.make_animation('images/coinChange.gif', delay=2)
+    sol = solve(coins, amount)
+
+    if sol == math.inf:
+        return 1
+    else:
+        return sol
