0c3198593cdfbde25c5bbf5758b36e4df989003d
namank03 <khandelwalnaman03@gmail.com>
2021-10-16 01:14:19 +0530

    word_bank wrong version

---
+++ blind_75/word_bank.py
@@ -0,0 +1,16 @@
+
+def word_bank(target, bank, op, memo={}):
+    if op == target:
+        return True
+
+    if len(op) > len(target):
+        return False
+
+    if op in memo:
+        return memo[op]
+
+    memo[op] = any(word_bank(target, bank, op + i, memo) for i in bank)
+    return memo[op]
+
+
+print(word_bank("leetcode",  ["leet", "code"], ""))
