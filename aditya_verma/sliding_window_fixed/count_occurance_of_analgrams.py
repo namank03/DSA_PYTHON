6878791f25ddd09fc03cc3665fe7b1048d6e9030
namank03 <khandelwalnaman03@gmail.com>
2021-10-16 17:20:28 +0530

    count analgams improved version with the count

--- aditya_verma/sliding_window_fixed/count_occurance_of_analgrams.py
+++ aditya_verma/sliding_window_fixed/count_occurance_of_analgrams.py
@@ -8,10 +8,17 @@ def count_occurrences(s, pat, n, k):
     g_r = 0
     c_r = Counter(pat)
 
+    # initialize the count with the counter size
+    count = len(c_r)
+
     while j < n:
         # Some init cal.
         if s[j] in c_r:
-            c_r[s[j]] = c_r.get(s[j]) - 1
+            c_r[s[j]] -= 1
+
+            # if char count goes to 0 then we need to decrease the total variable count
+            if c_r[s[j]] == 0:
+                count -= 1
 
         # make window of size k
         if j-i+1 < k:
@@ -19,12 +26,18 @@ def count_occurrences(s, pat, n, k):
 
         elif j-i+1 == k:
             # Do some other cal
-            if all(i == 0 for i in c_r.values()):
+            # When there's no element in the counter/map the count will go to 0
+            if count == 0:
                 print(s[i:j+1])
                 g_r += 1
 
+            #  as we move forward we need to inc the count of the element at postition i if it's decreased before.
             if s[i] in c_r:
                 c_r[s[i]] += 1
+
+            #  we inc the variable count only once and if there's a duplicate element the if condition will be ignored.
+            if c_r[s[i]] == 1:
+                count += 1
             # Remove the ith element from the window cal and then move the window
             i += 1
             j += 1
@@ -32,4 +45,4 @@ def count_occurrences(s, pat, n, k):
     return g_r
 
 
-print(count_occurrences('aabaabaa', 'aaba', 8, 4))
+print(count_occurrences('aabanabaa', 'aaba', 9, 4))
