2a5b105d51bf7b1e439d2032a04ed7dcc89e514d
namank03 <khandelwalnaman03@gmail.com>
2022-01-08 23:07:16 +0530

    Added djiktras algo

---
+++ graphs/djiktras.py
@@ -0,0 +1,27 @@
+import heapq as heap
+from collections import defaultdict
+
+
+def dijkstra(G, startingNode):
+    visited = set()
+    parentsMap = {}
+    pq = []
+    nodeCosts = defaultdict(lambda: float('inf'))
+    nodeCosts[startingNode] = 0
+    heap.heappush(pq, (0, startingNode))
+
+    while pq:
+        _, node = heap.heappop(pq)
+        visited.add(node)
+
+        for adjNode, weight in G[node].items():
+            if adjNode in visited:
+                continue
+
+            newCost = nodeCosts[node] + weight
+            if nodeCosts[adjNode] > newCost:
+                parentsMap[adjNode] = node
+                nodeCosts[adjNode] = newCost
+                heap.heappush(pq, (newCost, adjNode))
+
+    return parentsMap, nodeCosts
