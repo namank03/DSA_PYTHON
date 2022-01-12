01d17304fbaf024b3eee49e7fc8c17d48e5ec819
namank03 <khandelwalnaman03@gmail.com>
2022-01-12 16:20:13 +0530

    basic heap implementation in js

---
+++ javascript/heap/basic_heap.js
@@ -0,0 +1,86 @@
+class Heap {
+  constructor(comparator = (a, b) => a - b) {
+    this.array = [];
+    this.comparator = (i1, i2) => {
+      const value = comparator(this.array[i1], this.array[i2]);
+      if (Number.isNaN(value)) {
+        throw new Error(
+          `Comparator should evaluate to a number. Got ${value} when comparing ${this.array[i1]} with ${this.array[i2]}`
+        );
+      }
+      return value;
+    };
+  }
+
+  add(value) {
+    this.array.push(value);
+    this.bubbleUp();
+  }
+
+  peek() {
+    return this.array[0];
+  }
+
+  remove(index = 0) {
+    if (!this.size) return null;
+    this.swap(index, this.size - 1); // swap with last
+    const value = this.array.pop(); // remove element
+    this.bubbleDown(index);
+    return value;
+  }
+
+  get size() {
+    return this.array.length;
+  }
+
+  bubbleUp() {
+    let index = this.size - 1;
+    const parent = (i) => Math.ceil(i / 2 - 1);
+    while (parent(index) >= 0 && this.comparator(parent(index), index) > 0) {
+      this.swap(parent(index), index);
+      index = parent(index);
+    }
+  }
+
+  bubbleDown(index = 0) {
+    let curr = index;
+    const left = (i) => 2 * i + 1;
+    const right = (i) => 2 * i + 2;
+    const getTopChild = (i) =>
+      right(i) < this.size && this.comparator(left(i), right(i)) > 0
+        ? right(i)
+        : left(i);
+
+    while (
+      left(curr) < this.size &&
+      this.comparator(curr, getTopChild(curr)) > 0
+    ) {
+      const next = getTopChild(curr);
+      this.swap(curr, next);
+      curr = next;
+    }
+  }
+
+  swap(i1, i2) {
+    [this.array[i1], this.array[i2]] = [this.array[i2], this.array[i1]];
+  }
+}
+
+// aliases
+Heap.prototype.poll = Heap.prototype.remove;
+Heap.prototype.offer = Heap.prototype.add;
+Heap.prototype.element = Heap.prototype.peek;
+
+// module.exports = Heap;
+
+const heap = new Heap();
+heap.add(9);
+heap.add(19);
+heap.add(21);
+heap.add(0);
+heap.add(90);
+console.log(`heap`, heap);
+
+while (heap.size > 0) {
+  console.log(`heap.pop()`, heap.remove());
+}
