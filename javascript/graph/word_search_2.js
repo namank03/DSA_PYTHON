5be62a5d2aeab7063ed5e80f2f35c8265cec95e3
namank03 <khandelwalnaman03@gmail.com>
2022-01-16 21:05:38 +0530

    word_search_2 javascript implementation

--- javascript/graph/word_search_2.js
+++ javascript/graph/word_search_2.js
@@ -18,3 +18,48 @@ class TrieNode {
 
 trie = new TrieNode();
 trie.insert("Naman");
+trie.insert("Name");
+trie.insert("rand");
+
+const graph = [
+  ["N", "a", "m", "e"],
+  ["a", "b", "a", "d"],
+  ["c", "d", "n", "p"],
+  ["c", "d", "n", "p"],
+];
+
+const dfs = (i, j, trie) => {
+  if (trie.isWord) {
+    console.log(`trie.isWord`, trie.isWord);
+    trie.isWord = false;
+  }
+  if (i < 0 || i >= graph.length || j < 0 || j >= graph[0].length) {
+    return;
+  }
+
+  tmp = graph[i][j];
+  trie = trie.children[tmp];
+  if (!trie) return;
+  graph[i][j] = "#";
+  for (let [dx, dy] of [
+    [1, 0],
+    [0, 1],
+    [-1, 0],
+    [0, -1],
+  ]) {
+    dfs(i + dx, j + dy, trie);
+  }
+  graph[i][j] = tmp;
+};
+
+const solve = (graph, trie) => {
+  for (let i = 0; i < graph.length; i++) {
+    for (let j = 0; j < graph[0].length; j++) {
+      if (graph[i][j] in trie.children) {
+        dfs(i, j, trie);
+      }
+    }
+  }
+};
+
+solve(graph, trie);
